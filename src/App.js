60db3501b17f797b58aebc90a12aaa7b5a49811c
Taryn Martin <taryn.martin919@gmail.com>
2020-08-20 16:51:04 -0600

    Add POST request for login and code to change page view #15 #16

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header'
 import Movies from './Movies'
+import Login from './Login'
 // import MovieCard from './MovieCard'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      pageView: 'home',
       movies: [],
       error: '',
+      login: false,
+      userId: null,
+      userName: '',
+      userEmail: '',
     }
   }
   componentDidMount() {
@@ -19,12 +25,62 @@ class App extends Component {
       .catch(error => this.setState({ error: "STELLLLAAAA"}));
   }
 
+  submitPostRequest = (loginInfo) => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/login', {
+      method: 'POST',
+      headers: {
+      	'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginInfo),
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          pageView: 'loggedIn',
+          login: true,
+          userId: json.id,
+          userName: json.name,
+          userEmail: json.email,
+        });
+      })
+      // welcome for user and logout button once logged in?
+      .catch(err => {
+        this.setState({ loginError: 'Oh no! Please enter a valid email and password to login.'});
+        console.log('it failed');
+      });
+  }
+
+  showLogin = () => {
+    this.setState({pageView: 'login'})
+  }
+
   render() {
+    const page = this.state.pageView;
     return (
       <main className="App">
-        <Header />
-        <Movies movies={this.state.movies}/>
-        {this.state.error && <h2>{this.state.error}</h2>}
+      <header className="App-header">
+        <h1>Rancid Tomatillos</h1>
+        {page === 'loggedin' &&
+          <h2>Hello {this.state.userName}!</h2>
+        }
+        <button className='login-btn' onClick={this.showLogin}>Login</button>
+      </header>
+      {page === 'home' &&
+        <Movies
+          movies={this.state.movies}
+          error={this.state.error}
+          />
+      }
+      {page === 'login' &&
+        <Login submitLogin={this.submitPostRequest}/>
+      }
+      {
+        page === 'loggedIn' &&
+        <Movies
+          movies={this.state.movies}
+          error={this.state.error}
+        />
+      }
       </main>
     )
   }
