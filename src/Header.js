c9b7ad852cf4a9110556425f4dba92a732fea3fb
Taryn Martin <taryn.martin919@gmail.com>
2020-08-20 17:26:16 -0600

    Refactor Header component to use props and call a method in App #17

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './Header.css';
+import Login from './Login';
 
 class Header extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
-
   render() {
     return (
       <header className="App-header">
         <h1>Rancid Tomatillos</h1>
-        <button>Login</button>
+        <button onClick={this.props.loginBtn}>Login</button>
       </header>
     )
   }
