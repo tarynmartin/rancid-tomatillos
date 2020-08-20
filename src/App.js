d7a1f15f6ff44225e4ebb297e3b7ee9504201181
Taryn Martin <taryn.martin919@gmail.com>
2020-08-20 09:34:48 -0600

    Fix URL to be correct

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    fetch("https:rancid-tomatillos.herokuapp.com/api/v2/movie")
+    fetch("https:rancid-tomatillos.herokuapp.com/api/v2/movies")
       .then(response => response.json())
       .then(data => this.setState({movies: data.movies}))
       .catch(error => this.setState({ error: "STELLLLAAAA"}));
