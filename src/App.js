e60cf75db2f81e7fb4907d79582056a3fe0d3a29
Taryn Martin <taryn.martin919@gmail.com>
2020-08-20 09:33:23 -0600

    Fix catch on fetch to work correctly #2

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ class App extends Component {
     super();
     this.state = {
       movies: [],
-      error: ''
+      error: '',
     }
   }
   componentDidMount() {
-    fetch("https:rancid-tomatillos.herokuapp.com/api/v2/movies")
+    fetch("https:rancid-tomatillos.herokuapp.com/api/v2/movie")
       .then(response => response.json())
       .then(data => this.setState({movies: data.movies}))
-      .catch(error => this.state.error = 'STELLLLLLAAAAAA');
+      .catch(error => this.setState({ error: "STELLLLAAAA"}));
   }
 
   render() {
     return (
       <main className="App">
         <Header />
-        {this.state.error && <h2>{this.state.error}</h2>}
+        {this.state.error && <h2>Hello World!</h2>}
       </main>
     )
   }
