e3aa20f6cb034abfc534690596660d6db227cb2c
Taryn Martin <taryn.martin919@gmail.com>
2020-08-21 10:31:39 -0600

    Create test for MovieCard component #21

---
+++ src/MovieCard.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  MovieCard from './MovieCard';
+import { screen, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('MovieCard component', () => {
+  it('should have the correct content on movie card', () => {
+    render(<MovieCard
+      movie={{"id":1,"poster_path":"https://image.tmdb.org.jpg","backdrop_path":"https://image.tmdb.org/.jpg","title":"Greenland","average_rating":9,"release_date":"2020-07-29"}}
+      />);
+
+    screen.debug();
+
+    const title = screen.getByText('Greenland');
+    const average_rating = screen.getByText("9");
+
+    expect(title).toBeInTheDocument();
+    expect(average_rating).toBeInTheDocument();
+  })
+})
