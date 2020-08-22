1e8b8c6b21e7d9caba394c587732d4febf576f19
Taryn Martin <taryn.martin919@gmail.com>
2020-08-22 16:17:07 -0600

    Create outline for App component test #19

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App', () => {
+  it('displays the login button & Movies Screen', () => {
+    render(<App />);
+
+
+  });
+  it('displays the login screen on click', () => {
+    render(<App />);
+
+
+  });
+  it('displays the logout button & Movies Screen on login', () => {
+    render(<App />);
+
+
+  });
+  // it('displays movie information when card is clicked', () => {
+  //   render(<App />);
+  //
+  //
+  // });
 });
