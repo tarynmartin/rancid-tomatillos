b0a66e842c5aa4246fc8522983018555d2128aa2
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-24 18:54:15 -0600

    Change avgRating to average_rating to properly return expected data

--- src/MovieShow.js
+++ src/MovieShow.js
@@ -3,16 +3,16 @@ import React from 'react';
 const MovieShow = (props) => {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <h3>Average Rating: {props.avgRating}</h3>
-      <img src={props.poster} className="movie-poster"/>
-      <h2>{props.tagline}</h2>
-      <h3>Runtime: {props.runtime} minutes</h3>
-      <h3>Release Date: {props.releaseDate}</h3>
-      <p>Genres: {props.genres}</p>
-      <p>{props.overview}</p>
-      <h4>Budget: ${props.budget}</h4>
-      <h4>Revenue: ${props.revenue}</h4>
+      <h1>{props.movie.title}</h1>
+      <h3>Average Rating: {props.movie.average_rating}</h3>
+      <img src={props.movie.poster} className="movie-poster"/>
+      <h2>{props.movie.tagline}</h2>
+      <h3>Runtime: {props.movie.runtime} minutes</h3>
+      <h3>Release Date: {props.movie.release_date}</h3>
+      <p>Genres: {props.movie.genres}</p>
+      <p>{props.movie.overview}</p>
+      <h4>Budget: ${props.movie.budget}</h4>
+      <h4>Revenue: ${props.movie.revenue}</h4>
     </div>
   )
 }
