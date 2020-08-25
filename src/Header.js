c7f1c0a24be167f76a638cc57f6b10d854a595c3
Taryn Martin <taryn.martin919@gmail.com>
2020-08-25 14:47:22 -0600

    Add code to show logout button for logged in user when they are looking at one movie's info #40

--- src/Header.js
+++ src/Header.js
@@ -13,6 +13,13 @@ class Header extends Component {
           <button onClick={this.props.logoutBtn}>Log Out</button>
         </header>
       )
+    } else if (this.props.pageView === 'movie-show' && this.props.user != '') {
+      return (
+        <header className="App-header">
+          <h1>Rancid Tomatillos</h1>
+          <button onClick={this.props.logoutBtn}>Log Out</button>
+        </header>
+      )
     } else if (this.props.pageView === 'login') {
       return (
         <header className="App-header">
