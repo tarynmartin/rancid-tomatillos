3cddd97d400493af3820825027615367a230f86a
Taryn Martin <taryn.martin919@gmail.com>
2020-08-26 17:03:39 -0600

    Add input for rating that is only shown when a user is logged in #30

--- src/MovieShow.js
+++ src/MovieShow.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
 const MovieShow = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <h3>Average Rating: {props.average_rating}</h3>
-      <img src={props.poster} className="movie-poster"/>
-      <h2>{props.tagline}</h2>
-      <h3>Runtime: {props.runtime} minutes</h3>
-      <h3>Release Date: {props.release_date}</h3>
-      <p>Genres: {props.genres}</p>
-      <p>{props.overview}</p>
-      <h4>Budget: ${props.budget}</h4>
-      <h4>Revenue: ${props.revenue}</h4>
-    </div>
-  )
+  if (props.loggedIn === false) {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <h3>Average Rating: {props.avgRating}</h3>
+        <img src={props.poster} className="movie-poster"/>
+        <h2>{props.tagline}</h2>
+        <h3>Runtime: {props.runtime} minutes</h3>
+        <h3>Release Date: {props.releaseDate}</h3>
+        <p>Genres: {props.genres}</p>
+        <p>{props.overview}</p>
+        <h4>Budget: ${props.budget}</h4>
+        <h4>Revenue: ${props.revenue}</h4>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <h3>Average Rating: {props.avgRating}</h3>
+        <h3>Rate This Movie from 1-10!<input type='number'/></h3>
+        <img src={props.poster} className="movie-poster"/>
+        <h2>{props.tagline}</h2>
+        <h3>Runtime: {props.runtime} minutes</h3>
+        <h3>Release Date: {props.releaseDate}</h3>
+        <p>Genres: {props.genres}</p>
+        <p>{props.overview}</p>
+        <h4>Budget: ${props.budget}</h4>
+        <h4>Revenue: ${props.revenue}</h4>
+      </div>
+    )
+  }
 }
 
 export default MovieShow;
