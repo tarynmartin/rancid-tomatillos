f23fed3484e7d8cc90dbe54e8c6bdac275072ee0
Taryn Martin <taryn.martin919@gmail.com>
2020-08-26 17:14:16 -0600

    Made component a class to better post/get ratings information #30

--- src/MovieShow.js
+++ src/MovieShow.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovieShow = (props) => {
-  if (props.loggedIn === false) {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        <h3>Average Rating: {props.avgRating}</h3>
-        <img src={props.poster} className="movie-poster"/>
-        <h2>{props.tagline}</h2>
-        <h3>Runtime: {props.runtime} minutes</h3>
-        <h3>Release Date: {props.releaseDate}</h3>
-        <p>Genres: {props.genres}</p>
-        <p>{props.overview}</p>
-        <h4>Budget: ${props.budget}</h4>
-        <h4>Revenue: ${props.revenue}</h4>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        <h3>Average Rating: {props.avgRating}</h3>
-        <h3>Rate This Movie from 1-10!<input type='number'/></h3>
-        <img src={props.poster} className="movie-poster"/>
-        <h2>{props.tagline}</h2>
-        <h3>Runtime: {props.runtime} minutes</h3>
-        <h3>Release Date: {props.releaseDate}</h3>
-        <p>Genres: {props.genres}</p>
-        <p>{props.overview}</p>
-        <h4>Budget: ${props.budget}</h4>
-        <h4>Revenue: ${props.revenue}</h4>
-      </div>
-    )
+class MovieShow extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      userRating: null,
+    }
+  }
+
+  render() {
+    if (this.props.loggedIn === false) {
+      return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <h3>Average Rating: {this.props.avgRating}</h3>
+          <img src={this.props.poster} className="movie-poster"/>
+          <h2>{this.props.tagline}</h2>
+          <h3>Runtime: {this.props.runtime} minutes</h3>
+          <h3>Release Date: {this.props.releaseDate}</h3>
+          <p>Genres: {this.props.genres}</p>
+          <p>{this.props.overview}</p>
+          <h4>Budget: ${this.props.budget}</h4>
+          <h4>Revenue: ${this.props.revenue}</h4>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <h3>Average Rating: {this.props.avgRating}</h3>
+          <h3>Your Rating: {this.props.userRating}</h3>
+          <h3>Rate This Movie from 1-10!<input type='number'/><button>Submit</button></h3>
+          <img src={this.props.poster} className="movie-poster"/>
+          <h2>{this.props.tagline}</h2>
+          <h3>Runtime: {this.props.runtime} minutes</h3>
+          <h3>Release Date: {this.props.releaseDate}</h3>
+          <p>Genres: {this.props.genres}</p>
+          <p>{this.props.overview}</p>
+          <h4>Budget: ${this.props.budget}</h4>
+          <h4>Revenue: ${this.props.revenue}</h4>
+        </div>
+      )
+    }
   }
 }
+
+
 
 export default MovieShow;
