2d1640edcd01c87cfbbf35e8762039cd6ced6cdf
Taryn Martin <taryn.martin919@gmail.com>
2020-08-26 18:05:25 -0600

    Create outline for submitting a rating for a user #30

--- src/MovieShow.js
+++ src/MovieShow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { submitUserRating } from './apiCalls';
 
 class MovieShow extends Component{
   constructor(props) {
@@ -6,6 +7,35 @@ class MovieShow extends Component{
     this.state = {
       userRating: null,
     }
+  }
+  // make sure input is between 1-10
+  submitRating = (event) => {
+    event.preventDefault();
+    const newRating = {
+      movie_id: this.props.movieId,
+      rating: this.state.userRating
+    }
+    //need to finish the POST and store the returned rating data in state to display
+    // create a catch for it
+
+    submitUserRating(this.props.userId, newRating);
+    this.clearInputs();
+  }
+
+  clearInputs() {
+    this.setState({userRating: null})
+  }
+
+  // submitRating = (id, userRating) => {
+  //   this.submitUserRating(this.props.userId, newRating);
+  //   this.clearInputs();
+  // }
+  //find movie id in user ratings, if found, return error message
+
+  createRating = (event) => {
+    const inputKey = event.target.name;
+    const inputValue = event.target.value;
+    this.setState({[inputKey]: inputValue})
   }
 
   render() {
@@ -30,7 +60,15 @@ class MovieShow extends Component{
           <h1>{this.props.title}</h1>
           <h3>Average Rating: {this.props.avgRating}</h3>
           <h3>Your Rating: {this.props.userRating}</h3>
-          <h3>Rate This Movie from 1-10!<input type='number'/><button>Submit</button></h3>
+          <h3>Rate This Movie from 1-10!
+            <input
+            type='number'
+            name='userRating'
+            value={this.state.userRating}
+            onChange={this.createRating}
+            />
+            <button onClick={this.submitRating}>Submit</button>
+          </h3>
           <img src={this.props.poster} className="movie-poster"/>
           <h2>{this.props.tagline}</h2>
           <h3>Runtime: {this.props.runtime} minutes</h3>
