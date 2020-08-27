7f3031403e3538298224bcf078ca8ed421a98f42
Taryn Martin <taryn.martin919@gmail.com>
2020-08-26 18:05:44 -0600

    Add a post request for submitting a ratin #30

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -23,3 +23,15 @@ export const retrieveUserRatings = (userID) => {
   return fetch(`https:rancid-tomatillos.herokuapp.com/api/v2/users/${userID}/ratings`)
     .then(response => response.json())
 }
+
+export const submitUserRating = (userID, ratingInfo) => {
+  return fetch(`https:rancid-tomatillos.herokuapp.com/api/v2/users/${userID}/ratings`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(ratingInfo),
+    })
+      .then(response => response.json())
+
+}
