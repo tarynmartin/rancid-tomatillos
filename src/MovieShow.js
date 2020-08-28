1ec142da3c5f1f9f4b939b32c14c1071f3b579d6
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-27 21:44:43 -0600

    Add back button to movie-show page

--- src/MovieShow.js
+++ src/MovieShow.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const MovieShow = (props) => {
   return (
     <div>
+      <NavLink exact to='/'>Back</NavLink>
       <h1>{props.title}</h1>
       <h3>Average Rating: {props.avgRating}</h3>
       <img src={props.poster} className="movie-poster"/>
