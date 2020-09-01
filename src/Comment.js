52b6b8fb8fa540a391e18a631c453f1132792031
Taryn Martin <taryn.martin919@gmail.com>
2020-09-01 08:30:48 -0600

    Add code to create a comment card

---
+++ src/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = (props) => {
+  return (
+    <article className='comment-card'>
+      <h3>{props.author}</h3>
+      <p>{props.comment}</p>
+    </article>
+  )
+}
