ef3f9af7b38673ab53c2a7eb08c5db0e1bb72caf
Taryn Martin <taryn.martin919@gmail.com>
2020-09-07 17:24:49 -0600

    Refactor MovieCard component to be functional

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -3,30 +3,18 @@ import PropTypes from 'prop-types';
 import './MovieCard.css';
 import { Link } from 'react-router-dom'
 
-class MovieCard extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      movieId: this.props.movie.id
-    }
-  }
-
-  showMovie = () => {
-    const { showMovieInfo } = this.props;
-    showMovieInfo(this.state.movieId);
-  }
-  render() {
-    const { movie } = this.props;
-    return (
-      <Link to={'/' + movie.id}>
-        <div className="movie-card" onClick={this.showMovie}>
-          <p className='title'>{movie.title}</p>
-          <img src={movie.poster_path} className="movie-poster" alt="movie poster"/>
-          <p className='rating short-title'>Average Rating: {movie.average_rating.toFixed(2)}</p>
-        </div>
-      </Link>)
-  }
+const MovieCard = ({movie, showMovieInfo}) => {
+  return (
+    <Link to={'/' + movie.id}>
+      <div className="movie-card" onClick={() => showMovieInfo(movie.id)}>
+        <p className='title'>{movie.title}</p>
+        <img src={movie.poster_path} className="movie-poster" alt="movie poster"/>
+        <p className='rating short-title'>Average Rating: {movie.average_rating.toFixed(2)}</p>
+      </div>
+    </Link>
+  )
 }
+
 
 MovieCard.propTypes = {
   movieId: PropTypes.string,
