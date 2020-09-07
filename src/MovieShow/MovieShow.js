ab396e4514894068b8970e43f2ad915d0fce6c77
Taryn Martin <taryn.martin919@gmail.com>
2020-09-07 09:47:51 -0600

    Refactor MovieShow to be cleaner and take less props from App

--- src/MovieShow/MovieShow.js
+++ src/MovieShow/MovieShow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { submitUserRating, deleteUserRating } from '../helpers/apiCalls';
+import { retrieveMovieInfo, submitUserRating, deleteUserRating } from '../helpers/apiCalls';
 import Comments from '../Comments/Comments';
 import './MovieShow.css';
 import PropTypes from 'prop-types';
@@ -9,6 +9,17 @@ class MovieShow extends Component{
   constructor(props) {
     super(props);
     this.state = {
+      title: '',
+      poster: '',
+      backdrop: '',
+      releaseDate: '',
+      overview: '',
+      genres: null,
+      budget: null,
+      revenue: null,
+      runtime: null,
+      tagline: '',
+      avgRating: null,
       userRating: this.props.userRating || '',
       error: '',
       inputVisible: '' || this.props.ratingMatch,
@@ -20,37 +31,50 @@ class MovieShow extends Component{
       ratingId: null
     }
   }
-  componentDidUpdate(prevProps) {
-    const { movieId, userId, userRatings } = this.props;
-    if (userRatings !== prevProps) {
-      if (this.state.ratingDeleted) {
-        this.sendDeleteRating(userId, this.state.ratingId)
-      } else if(this.state.ratingSubmitted) {
-        const newRating = {
-          movie_id: movieId,
-          rating: parseInt(this.state.userRating)
-        }
 
-        this.sendPostRequest(newRating);
-      }
-    }
+  componentDidMount() {
+    const { movieId } = this.props;
+    this.getMovieInfo(movieId);
+  }
+
+  getMovieInfo = (movieId) => {
+    retrieveMovieInfo(movieId)
+      .then(data => this.setState({
+        title: data.movie.title,
+        poster: data.movie.poster_path,
+        backdrop: data.movie.backdrop_path,
+        releaseDate: data.movie.release_date,
+        overview: data.movie.overview,
+        genres: data.movie.genres.join(', '),
+        budget: data.movie.budget.toLocaleString(),
+        revenue: data.movie.revenue.toLocaleString(),
+        runtime: data.movie.runtime,
+        tagline: data.movie.tagline,
+        avgRating: data.movie.average_rating.toFixed(2),}))
+      .catch(error => this.setState({ pageView: 'home', error: "Sorry, we couldn't find that movie"}));
   }
 
   submitRating = (event) => {
+    const { movieId } = this.props;
     event.preventDefault();
     if (this.state.checkedInput === false) {
       return alert('Please enter a valid number from 1 - 10, no decimals!')
     } else {
-      this.setState({ratingSubmitted: true});
+      const newRating = {
+              movie_id: movieId,
+              rating: parseInt(this.state.userRating)
+            }
+      this.setState({ratingSubmitted: true}, () => this.sendPostRequest(newRating));
     }
   }
 
   sendPostRequest = (userRating) => {
-    const { movieId, userId, userRatings, changeAfterSubmit } = this.props;
+    const { movieId, userId, userRatings, getRatings } = this.props;
     submitUserRating(userId, userRating)
-      .then(newRating => changeAfterSubmit(userId, movieId))
+      .then(newRating => getRatings(userId))
+      .then(getMovie => this.getMovieInfo(movieId))
       .then(newResponse => this.setState({inputVisible: 'hidden', deleteVisible: 'delete-button', userRatings: userRatings}))
-      .then(finalResponse => this.setState({ratingSubmitted: false, checkedInput: false}))
+      .then(finalResponse => this.setState({checkedInput: false}))
       .catch(error => {
         this.setState({error: 'You have already submitted a rating for this movie.'})
       })
@@ -76,17 +100,18 @@ class MovieShow extends Component{
   }
 
   deleteRating = (event) => {
-    const { userRatings } = this.props;
+    const { userId, userRatings } = this.props;
     event.preventDefault();
     const ratingId = this.findRatingId(userRatings);
-    this.setState({ratingDeleted: true, ratingId: ratingId})
+    this.setState({ratingDeleted: true, ratingId: ratingId}, () => this.sendDeleteRating(userId, this.state.ratingId))
   }
 
   sendDeleteRating= (userId, ratingId) => {
     const {movieId, userRatings, getRatings} = this.props;
     deleteUserRating(userId, ratingId)
-      .then(response => getRatings(userId, movieId))
-      .then(newResponse => this.setState((state, props) => ({ratingId: null,userRating: null, deleteVisible: 'hidden', inputVisible: '', userRatings: userRatings})))
+      .then(response => getRatings(userId))
+      .then(getMovie => this.getMovieInfo(movieId))
+      .then(newResponse => this.setState((state, props) => ({ratingId: null, userRating: null, deleteVisible: 'hidden', inputVisible: '', userRatings: userRatings})))
       .then(finalResponse => this.setState({ratingDeleted: false}))
       .catch(error => {
         this.setState({error: 'Sorry, we were unable to remove your rating for this movie. Try again later!'})
@@ -101,80 +126,53 @@ class MovieShow extends Component{
   }
 
   render() {
-    const { movieId, loggedIn, title, poster, releaseDate, overview, genres, budget, revenue, runtime, tagline, avgRating, changePage } = this.props;
-    if (this.state.error !== "") {
-      return (
-        <div className='message-bar'>
-          <h2>{this.state.error}</h2>
-        </div>
-      )
-    } else if (loggedIn === false) {
+    const { movieId, userId, changePage } = this.props;
       return (
         <div className='movies-comments'>
           <div className='movie-show'>
             <div className='btn-box'>
               <NavLink className='back-btn' onClick={changePage} exact to='/'>Back</NavLink>
-              <h1 className='movie-title'>{title}</h1>
+              {this.state.error !== "" &&
+              <div className='message-bar'>
+                <h2>{this.state.error}</h2>
+              </div>
+              }
+              <h1 className='movie-title'>{this.state.title}</h1>
             </div>
             <div className='poster-display'>
-              <img src={poster} className="movie-poster" alt='movie poster'/>
-              <h2>{tagline}</h2>
-              <h3>Runtime: {runtime} minutes</h3>
+              <img src={this.state.poster} className="movie-poster" alt='movie poster'/>
+              <h2>{this.state.tagline}</h2>
+              <h3>Runtime: {this.state.runtime} minutes</h3>
             </div>
             <div className='movie-info'>
-              <h3>Average Rating: {avgRating}</h3>
-              <h3>Release Date: {releaseDate}</h3>
-              <p>Genres: {genres}</p>
-              <p className='overview'>{overview}</p>
-              <h4>Budget: ${budget}</h4>
-              <h4>Revenue: ${revenue}</h4>
+              <h3>Average Rating: {this.state.avgRating}</h3>
+              {userId !== null &&
+                <div>
+                <h3>Your Rating: {this.state.userRating}<button className={this.state.deleteVisible} onClick={this.deleteRating}>Delete Your Rating</button></h3>
+                <h3 className={this.state.inputVisible}>Rate This Movie from 1-10!</h3>
+                  <input
+                  className='rating-input'
+                  type='number'
+                  name='userRating'
+                  placeholder='1 - 10'
+                  value={this.state.userRating}
+                  onChange={this.createRating}
+                  />
+                  <button className='rating-btn' onClick={this.submitRating}>Submit</button>
+                </div>
+              }
+              <h3>Release Date: {this.state.releaseDate}</h3>
+              <p>Genres: {this.state.genres}</p>
+              <p className='overview'>{this.state.overview}</p>
+              <h4>Budget: ${this.state.budget}</h4>
+              <h4>Revenue: ${this.state.revenue}</h4>
             </div>
           </div>
           <Comments />
         </div>
       )
-    } else {
-      return (
-        <div className='movies-comments'>
-          <div className='movie-show'>
-            <div className='btn-box'>
-              <NavLink className='back-btn' onClick={changePage} exact to='/'>Back</NavLink>
-              <h1 className='movie-title'>{title}</h1>
-            </div>
-            <div className='poster-display'>
-              <img src={poster} className="movie-poster" alt='movie poster'/>
-              <h2>{tagline}</h2>
-              <h3>Runtime: {runtime} minutes</h3>
-            </div>
-            <div className='movie-info'>
-              <h3 className='avg-rating'>Average Rating: {avgRating}</h3>
-              <h3>Your Rating: {this.state.userRating}<button className={this.state.deleteVisible} onClick={this.deleteRating}>Delete Your Rating</button></h3>
-              <h3 className={this.state.inputVisible}>Rate This Movie from 1-10!</h3>
-                <input
-                className='rating-input'
-                type='number'
-                name='userRating'
-                placeholder='1 - 10'
-                value={this.state.userRating}
-                onChange={this.createRating}
-                />
-                <button className='rating-btn' onClick={this.submitRating}>Submit</button>
-              <h3>Release Date: {releaseDate}</h3>
-              <p>Genres: {genres}</p>
-              <p className='overview'>{overview}</p>
-              <h4>Budget: ${budget}</h4>
-              <h4>Revenue: ${revenue}</h4>
-            </div>
-          </div>
-          <Comments
-            login={loggedIn}
-            movieId={movieId}
-          />
-        </div>
-      )
     }
   }
-}
 
 MovieShow.propTypes = {
   error: PropTypes.string,
@@ -185,7 +183,6 @@ MovieShow.propTypes = {
   ratingId: PropTypes.number,
   movieId: PropTypes.number,
   userId: PropTypes.number,
-  loggedIn: PropTypes.bool,
   title: PropTypes.string,
   poster: PropTypes.string,
   releaseDate: PropTypes.string,
@@ -201,7 +198,6 @@ MovieShow.propTypes = {
   userRating: PropTypes.string,
   deleteVisible: PropTypes.string,
   getRatings: PropTypes.func,
-  changeAfterSubmit: PropTypes.func,
   changePage: PropTypes.func
 }
 
